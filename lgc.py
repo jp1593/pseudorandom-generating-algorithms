b400f89703dd81b61afbf5888dc6dfa30ffa2eb4
jp1593 <juanpablo08082002@hotmail.com>
2025-08-31 17:41:12 -0600

    basic histogram implementation of lgc

--- lgc.py
+++ lgc.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 # LGC Function
 def LGC(seed, a, c, m, amount): 
     """
@@ -15,4 +18,8 @@ def LGC(seed, a, c, m, amount):
 pseudo_number = LGC(seed=10, a=22695477,c=1,m=2**31, amount=10)
 # Print of list items in a iterative way
 print(*pseudo_number, sep='\n')
+# Histogram
+data = pseudo_number
+plt.hist(data, bins=10)
+plt.show()
 
