577d8ee495eb4d6e67c5cb30a10572310472e61e
jp1593 <juanpablo08082002@hotmail.com>
2025-08-31 17:37:24 -0600

    lgc for multiple numbers

--- lgc.py
+++ lgc.py
@@ -1,15 +1,18 @@
 # LGC Function
-def LGC(seed, a, c, m): 
+def LGC(seed, a, c, m, amount): 
     """
     Generador de Congruencia Lineal (LCG)
     """
+    pseudo_list = []
     x = seed
-    prev_x = (a * x + c) % m
-    prev_x = (a * prev_x + c) % m
-
-    return prev_x
+    for _ in range(amount): 
+        prev_x = (a * x + c) % m
+        pseudo_list.append(prev_x / m)
+        x = prev_x
+    return pseudo_list
     
 # Using the Borland C/C++ parameters
-pseudo_number = LGC(seed=10, a=22695477,c=1,m=2**31)
-print(pseudo_number)
+pseudo_number = LGC(seed=10, a=22695477,c=1,m=2**31, amount=10)
+# Print of list items in a iterative way
+print(*pseudo_number, sep='\n')
 
