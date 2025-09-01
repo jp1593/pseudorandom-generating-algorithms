ce46622f10320fea7f1421f55688dd456e22d844
jp1593 <juanpablo08082002@hotmail.com>
2025-08-31 20:02:22 -0600

    numbers produce between 0-1 and comments

--- blumblum.py
+++ blumblum.py
@@ -1,11 +1,13 @@
+# Blum Blum Function
 def BlumBlum(seed, p, q, amount):
     x = seed
     m=p*q
     random_numbers = [] 
     for _ in range(amount): 
         x = (x*x) % m 
-        random_numbers.append(x)
+        random_numbers.append(x / m) # Normalization between [0,1]
     return random_numbers
 
+# Function call and print data
 blum_generation = BlumBlum(3, 11, 19, 10)
 print(blum_generation)
