58654fb19986d753b1b3742646145d16f1882c2a
jp1593 <juanpablo08082002@hotmail.com>
2025-08-31 20:33:47 -0600

    histogram for randu

--- randu.py
+++ randu.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 # Randu function
 def Randu(seed, amount): 
     m = 2**31
@@ -10,5 +12,14 @@ def Randu(seed, amount):
     return random_numbers
 
 # Call of function and number display
-random = Randu(33, 100)
+random = Randu(33, 1000)
 print(*random, sep='\n')
+
+#Histogram
+plt.figure(figsize=(8,5))
+plt.hist(random, bins=35, color='skyblue', edgecolor='black', alpha=0.7)
+plt.title("Histogram of Pseudo-random Numbers (RANDU)")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.grid(axis='y', alpha=0.3)
+plt.show()
