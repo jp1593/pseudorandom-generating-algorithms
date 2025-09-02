357bdd6f500558550db8995ffa55d4d634fe4560
jp1593 <juanpablo08082002@hotmail.com>
2025-09-02 16:58:57 -0600

    middle-square padding and histogram

--- middle-square.py
+++ middle-square.py
@@ -1,19 +1,31 @@
+import matplotlib.pyplot as plt
+
+# Middle Square function
 def middle_square(seed, amount):
     if (len(str(seed)) < 4): 
-        print("Debe de introducir minimo 4 digitos")
+        print("This algorithm requires 4 digits at least for execution")
     else:
         double_list = []
         x = seed
-        # for _ in range(amount):
-        distribution = (len(str(seed)) + 2 - 1) // 2
-        double_square = x * x
-        double_square_str = str(double_square)
-        remove_digits = double_square_str[distribution:-distribution]
-        x = int(remove_digits)
-        double_list.append(x)
+        for _ in range(amount):
+            distribution = (len(str(seed)) + 2 - 1) // 2
+            double_square = x * x
+            double_square_str = str(double_square)
+            double_square_str = double_square_str.rjust(len(str(seed)) * 2, '0')
+            remove_digits = double_square_str[distribution:-distribution]
+            x = int(remove_digits)
+            double_list.append(x)
         return double_list
     
-random_number = middle_square(12345, 10)
+# Function call and print of values
+random_number = middle_square(7604, 1000)
 print(random_number)
 
-
+# Histogram
+plt.figure(figsize=(8,5))
+plt.hist(random_number, bins=150, color='skyblue', edgecolor='black', alpha=0.7)
+plt.title("Histogram of Pseudo-random Numbers (Middle-Square)")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.grid(axis='y', alpha=0.3)
+plt.show()
