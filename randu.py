ada6deceb902cad86a2d27669c64c0bdb1d369ee
jp1593 <juanpablo08082002@hotmail.com>
2025-09-10 16:45:05 -0600

    randu hipotesis

--- randu.py
+++ randu.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from scipy.stats import chisquare
+import numpy as np
 
 # Randu function
 def Randu(seed, amount): 
@@ -23,3 +25,29 @@ plt.xlabel("Value")
 plt.ylabel("Frequency")
 plt.grid(axis='y', alpha=0.3)
 plt.show()
+
+# Hypothesis test: 
+bins = 35
+n = len(random)
+E = n / bins  
+
+# Stadistic Chi^2
+counts, _ = np.histogram(random, bins=bins, range=(0.0, 1.0))
+chi2_stat, p_val = chisquare(counts, f_exp=[E]*bins)
+
+# Critical values for df = 34
+chi2_95 = 48.602
+chi2_90 = 45.563
+
+print("\n=== Prueba de Uniformidad (Randu) ===")
+print(f"Chi² (scipy) = {chi2_stat:.4f}")
+print(f"\nComparación con tabla:")
+print(f"  chi²[0.95, 34] = {chi2_95}")
+print(f"  chi²[0.90, 34] = {chi2_90}")
+
+if chi2_stat < chi2_95:
+    print("\nA un nivel de confianza del 95%: no podemos rechazar la hipótesis nula de que los números generados provienen de una distribución uniforme.")
+elif chi2_stat < chi2_90:
+    print("\nA un nivel de confianza del 90%: no podemos rechazar la hipótesis nula de que los números generados provienen de una distribución uniforme, pero sí se rechaza al 95%.")
+else:
+    print("Según la prueba, se rechaza la hipótesis nula: los números generados no provienen de una distribución uniforme.")
